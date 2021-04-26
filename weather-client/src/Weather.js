b62db26e799b460daaf05e9bb1dbd5d3cf69225c
Gideon Crawley <gscrawley@gmail.com>
2021-04-25 23:46:24 -0700

    added CORS to graphql index in order to connect servers

---
+++ weather-client/src/Weather.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { gql } from '@apollo/client'
+import { client } from './index'
+
+function Weather() {
+    return (
+      <div className="Weather">
+        <form>
+          ...
+        </form>
+      </div>
+    );
+  }
+  
+  export default Weather
